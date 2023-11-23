87a2608d9e7e16ea257a5b0cc64d1728e821c2f8
Pavithrakanchana <74247443+Pavithrakanchana@users.noreply.github.com>
2023-11-23 13:51:59 +0530

    Created CRUD Project

---
+++ CrudUsingSpringBoot/CrudUsingSpringBoot/src/main/java/com/Crud/CrudUsingSpringBoot/CrudUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.Crud.CrudUsingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsingSpringBootApplication.class, args);
+	}
+
+}
---
+++ CrudUsingSpringBoot/CrudUsingSpringBoot/src/test/java/com/Crud/CrudUsingSpringBoot/CrudUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Crud.CrudUsingSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
